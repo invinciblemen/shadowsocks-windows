1afa21e38e8756f93a8d967442e53cae4c9cd721
shazhiwei <shazhiwei96@163.com>
2020-10-09 14:29:35 +0800

    Update CHANGES and bump version

--- shadowsocks-csharp/Controller/Service/UpdateChecker.cs
+++ shadowsocks-csharp/Controller/Service/UpdateChecker.cs
@@ -26,7 +26,7 @@ namespace Shadowsocks.Controller
         public string LatestVersionLocalName;
         public event EventHandler CheckUpdateCompleted;
 
-        public const string Version = "4.2.0.2";
+        public const string Version = "4.2.0.3";
 
         private class CheckUpdateTimer : System.Timers.Timer
         {
